0dcc7270eabde4722ca9ab873247ad87c15b54de
zhuima <zhuima314@gmail.com>
2024-06-05 11:30:01 +0800

    keep admin upload function

---
+++ app/admin/upload/page.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Author: zhuima zhuima314@gmail.com
+ * @Date: 2024-06-05 09:05:59
+ * @LastEditors: zhuima zhuima314@gmail.com
+ * @LastEditTime: 2024-06-05 11:29:42
+ * @FilePath: /PicImpact/app/admin/upload/page.tsx
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
+import FileUpload from "~/components/admin/upload/FileUpload";
+import DashHeader from "~/components/layout/DashHeader";
+
+export default function Upload() {
+  return <FileUpload />;
+}
