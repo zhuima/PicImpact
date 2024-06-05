5bfeccb05a46db92e6db3ecd2bc0ad91e25a4ec2
zhuima <zhuima314@gmail.com>
2024-06-05 14:50:25 +0800

    Anonymous uploads are prohibited

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
  * @Author: zhuima zhuima314@gmail.com
  * @Date: 2024-06-05 09:05:59
  * @LastEditors: zhuima zhuima314@gmail.com
- * @LastEditTime: 2024-06-05 13:20:12
+ * @LastEditTime: 2024-06-05 14:49:38
  * @FilePath: /PicImpact/middleware.ts
  * @Description:
  *
@@ -15,13 +15,13 @@ import { NextResponse } from "next/server";
 
 export default auth((req) => {
   // 直接通过 /api/v1/get-tags 请求，不进行认证
-  if (
-    req.nextUrl.pathname === "/api/v1/get-tags" ||
-    req.nextUrl.pathname === "/api/v1/file-upload" ||
-    req.nextUrl.pathname === "/api/v1/image-add"
-  ) {
-    return NextResponse.next();
-  }
+  // if (
+  //   req.nextUrl.pathname === "/api/v1/get-tags" ||
+  //   req.nextUrl.pathname === "/api/v1/file-upload" ||
+  //   req.nextUrl.pathname === "/api/v1/image-add"
+  // ) {
+  //   return NextResponse.next();
+  // }
 
   if (req.nextUrl.pathname.startsWith("/api/v1") && !req.auth) {
     return Response.json(
